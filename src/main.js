e006891231db476938c879c4b0dcccc8a6b77cca
Metryumora <metryumora@gmail.com>
2018-06-20 19:24:22 +0300

    The third task done.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,17 @@ function loadStudents() {
     getStudents().then(showStudents);
 }
 
-window.onload = loadStudents;
+window.onload = function () {
+    let form = document.getElementById("addStudentForm");
+    form.addEventListener("submit", ev => {
+        ev.preventDefault();
+        let name = document.getElementById("nameInput").value;
+        let grade = document.getElementById("gradeInput").value;
+        addStudent({fullName: name, grade: grade}).then(loadStudents);
+    });
+
+    loadStudents();
+};
 
 function getStudents() {
     return fetch(studentsURL).then(r => r.json());
@@ -28,6 +38,16 @@ function updateStudentTemplate(template, student) {
     studentName.innerText = student.fullName;
     let studentGrade = template.querySelector(".grade");
     studentGrade.innerText = student.grade;
+}
+
+function addStudent(student) {
+    return fetch(studentsURL, {
+        method: 'POST',
+        body: JSON.stringify(student),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
 
 
